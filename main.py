84778f0b880096153ffd8d91343a46fb08ea1713
LE HAI SON <haison19952013@gmail.com>
2023-05-13 21:35:43 +0900

    update downloads tab

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import streamlit as st
-from src import Financial_statement_analysis, Side, Overview_tab, FS_tab, Top, Price_history_tab, News_tab
+from src import Financial_statement_analysis, Side, Overview_tab, FS_tab, Top, Downloads_tab, News_tab
 
 if __name__ == '__main__':
     st.set_page_config(layout='wide')
     tickerSymbol, start_date, end_date = Side()
     data_obj = Financial_statement_analysis(tickerSymbol)
     Top(data_obj)
-    overview_tab, FS_analysis_tab, News_analysis_tab, Price_history_analysis_tab = st.tabs(["Overview",
+    Overview_st_tab, FS_analysis_st_tab, News_st_tab, Download_st_tab = st.tabs(["Overview",
                             "Financial Statement Analysis",
                             "News and Events",
-                            "Price History"])
-    Overview_tab(overview_tab,data_obj,start_date, end_date)
-    FS_tab(FS_analysis_tab,data_obj)
-    Price_history_tab(Price_history_analysis_tab,data_obj,start_date, end_date)
-    News_tab(News_analysis_tab,data_obj)
+                            "Downloads"])
+    Overview_tab(Overview_st_tab,data_obj,start_date, end_date)
+    FS_tab(FS_analysis_st_tab,data_obj)
+    Downloads_tab(Download_st_tab,data_obj,start_date, end_date)
+    News_tab(News_st_tab,data_obj)
     
--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@ from .top import Top
 from .side import Side
 from .overview_tab import Overview_tab
 from .FS_tab import FS_tab
-from .price_history_tab import Price_history_tab
+from .downloads_tab import Downloads_tab
 from .news_tab import News_tab
