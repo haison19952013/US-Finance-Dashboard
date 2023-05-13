ef6497f9654a1e2084d2e5c41d8e7b6d908edddf
LE HAI SON <haison19952013@gmail.com>
2023-05-13 22:53:28 +0900

    Change title

--- src/CF_tab.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from .utils import Time_series_plot
-
-def CF_tab(tab_obj,data_obj):
-    with tab_obj:
-        st.header('1. Cash Flow')
-
--- src/IS_tab.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from .utils import Time_series_plot
-
-def IS_tab(tab_obj,data_obj):
-    with tab_obj:
-        st.header('1. Income Statement')
-
